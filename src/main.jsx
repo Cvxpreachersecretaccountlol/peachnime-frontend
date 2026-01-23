8e3e41e122850a782aba7fef414deff59bb5a6d4
Cvxpreachersecretaccountlol <orginpreacher@gmail.com>
2026-01-23 17:27:21 +0800

    Add BrowserRouter to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { ToastContainer } from "react-toastify";
@@ -25,7 +26,9 @@ createRoot(document.getElementById("root")).render(
           pauseOnHover
           theme="dark"
         />
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </StrictMode>
     </ThemeProvider>
   </QueryClientProvider>
