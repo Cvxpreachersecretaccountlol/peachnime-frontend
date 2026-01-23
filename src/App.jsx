17aea313325555d94be6e91389139a251606cac4
Cvxpreachersecretaccountlol <orginpreacher@gmail.com>
2026-01-23 10:08:58 +0800

    Wrap app with AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import AdMavenOnly from "./components/AdMavenOnly";
 import AppContent from "./AppContent";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <AdMavenOnly />
       <AppContent />
-    </>
+    </AuthProvider>
   );
 };
 
